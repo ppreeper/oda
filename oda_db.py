536f348a1d2b0a96530d7cbd862f45dfa5374de5
Peter Preeper <ppreeper@gmail.com>
2023-07-21 12:20:20 -0600

    add destfile and configfile options to help direct the backup

--- oda_db.py
+++ oda_db.py
@@ -33,12 +33,14 @@ class DatabaseExists(Warning):
 
 
 def _create_empty_database(name):
-    db = odoo.sql_db.db_connect('postgres')
+    db = odoo.sql_db.db_connect("postgres")
     with closing(db.cursor()) as cr:
-        chosen_template = odoo.tools.config['db_template']
+        chosen_template = odoo.tools.config["db_template"]
         cr.execute(
-            "SELECT datname FROM pg_database WHERE datname = %s", (name,),
-            log_exceptions=False)
+            "SELECT datname FROM pg_database WHERE datname = %s",
+            (name,),
+            log_exceptions=False,
+        )
         if cr.fetchall():
             raise DatabaseExists("database %r already exists!" % (name,))
         else:
@@ -47,20 +49,21 @@ def _create_empty_database(name):
             cr._cnx.autocommit = True
 
             # 'C' collate is only safe with template0, but provides more useful indexes
-            collate = sql.SQL("LC_COLLATE 'C'" if chosen_template ==
-                              'template0' else "")
+            collate = sql.SQL(
+                "LC_COLLATE 'C'" if chosen_template == "template0" else ""
+            )
             cr.execute(
-                sql.SQL("CREATE DATABASE {} ENCODING 'unicode' {} TEMPLATE {}")
-                .format(
-                    sql.Identifier(name), collate,
-                    sql.Identifier(chosen_template)))
+                sql.SQL("CREATE DATABASE {} ENCODING 'unicode' {} TEMPLATE {}").format(
+                    sql.Identifier(name), collate, sql.Identifier(chosen_template)
+                )
+            )
 
     # TODO: add --extension=trigram,unaccent
     try:
         db = odoo.sql_db.db_connect(name)
         with db.cursor() as cr:
             cr.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
-            if odoo.tools.config['unaccent']:
+            if odoo.tools.config["unaccent"]:
                 cr.execute("CREATE EXTENSION IF NOT EXISTS unaccent")
                 # From PostgreSQL's point of view, making 'unaccent' immutable is incorrect
                 # because it depends on external data - see
@@ -79,14 +82,16 @@ def _drop_conn(cr, db_name):
     try:
         # PostgreSQL 9.2 renamed pg_stat_activity.procpid to pid:
         # http://www.postgresql.org/docs/9.2/static/release-9-2.html#AEN110389
-        pid_col = 'pid' if cr._cnx.server_version >= 90200 else 'procpid'
+        pid_col = "pid" if cr._cnx.server_version >= 90200 else "procpid"
 
         cr.execute(
             """SELECT pg_terminate_backend(%(pid_col)s)
                       FROM pg_stat_activity
                       WHERE datname = %%s AND
-                            %(pid_col)s != pg_backend_pid()""" %
-            {'pid_col': pid_col}, (db_name,))
+                            %(pid_col)s != pg_backend_pid()"""
+            % {"pid_col": pid_col},
+            (db_name,),
+        )
     except Exception:
         pass
 
@@ -97,7 +102,7 @@ def exp_drop(db_name):
     odoo.modules.registry.Registry.delete(db_name)
     odoo.sql_db.close_db(db_name)
 
-    db = odoo.sql_db.db_connect('postgres')
+    db = odoo.sql_db.db_connect("postgres")
     with closing(db.cursor()) as cr:
         # database-altering operations cannot be executed inside a transaction
         cr._cnx.autocommit = True
@@ -105,13 +110,13 @@ def exp_drop(db_name):
 
         try:
             cr.execute(
-                sql.SQL('DROP DATABASE IF EXISTS {}').format(
-                    sql.Identifier(db_name)))
+                sql.SQL("DROP DATABASE IF EXISTS {}").format(sql.Identifier(db_name))
+            )
         except Exception as e:
-            _logger.info('DROP DB: %s failed:\n%s', db_name, e)
+            _logger.info("DROP DB: %s failed:\n%s", db_name, e)
             raise Exception("Couldn't drop database %s: %s" % (db_name, e))
         else:
-            _logger.info('DROP DB: %s', db_name)
+            _logger.info("DROP DB: %s", db_name)
 
     fs = odoo.tools.config.filestore(db_name)
     if os.path.exists(fs):
@@ -126,65 +131,67 @@ def dump_db_manifest(cr):
     )
     modules = dict(cr.fetchall())
     manifest = {
-        'odoo_dump': '1',
-        'db_name': cr.dbname,
-        'version': odoo.release.version,
-        'version_info': odoo.release.version_info,
-        'major_version': odoo.release.major_version,
-        'pg_version': pg_version,
-        'modules': modules,
+        "odoo_dump": "1",
+        "db_name": cr.dbname,
+        "version": odoo.release.version,
+        "version_info": odoo.release.version_info,
+        "major_version": odoo.release.major_version,
+        "pg_version": pg_version,
+        "modules": modules,
     }
     return manifest
 
 
-def _dump_db(db_name, bkp_name, folder="./backups", backup_format='zip'):
+def _dump_db(db_name, bkp_name, folder="./backups", backup_format="zip"):
     """Dump database `db` into file-like object `stream` if stream is None
-    return a file object with the dump """
+    return a file object with the dump"""
     bkp_file = f"{bkp_name}.zip"
     file_path = os.path.join(folder, bkp_file)
-    with open(file_path, 'wb') as stream:
-        _logger.info('DUMP DB: %s format %s', db_name, backup_format)
+    with open(file_path, "wb") as stream:
+        _logger.info("DUMP DB: %s format %s", db_name, backup_format)
 
-        cmd = [find_pg_tool('pg_dump'), '--no-owner', db_name]
+        cmd = [find_pg_tool("pg_dump"), "--no-owner", db_name]
         env = exec_pg_environ()
 
-        if backup_format == 'zip':
+        if backup_format == "zip":
             with tempfile.TemporaryDirectory() as dump_dir:
                 filestore = odoo.tools.config.filestore(db_name)
                 if os.path.exists(filestore):
-                    shutil.copytree(filestore,
-                                    os.path.join(dump_dir, 'filestore'))
-                with open(os.path.join(dump_dir, 'manifest.json'), 'w') as fh:
+                    shutil.copytree(filestore, os.path.join(dump_dir, "filestore"))
+                with open(os.path.join(dump_dir, "manifest.json"), "w") as fh:
                     db = odoo.sql_db.db_connect(db_name)
                     with db.cursor() as cr:
                         json.dump(dump_db_manifest(cr), fh, indent=4)
-                cmd.insert(-1, '--file=' + os.path.join(dump_dir, 'dump.sql'))
+                cmd.insert(-1, "--file=" + os.path.join(dump_dir, "dump.sql"))
                 subprocess.run(
                     cmd,
                     env=env,
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.STDOUT,
-                    check=True)
+                    check=True,
+                )
                 if stream:
                     odoo.tools.osutil.zip_dir(
                         dump_dir,
                         stream,
                         include_dir=False,
-                        fnct_sort=lambda file_name: file_name != 'dump.sql')
+                        fnct_sort=lambda file_name: file_name != "dump.sql",
+                    )
                 else:
                     t = tempfile.TemporaryFile()
                     odoo.tools.osutil.zip_dir(
                         dump_dir,
                         t,
                         include_dir=False,
-                        fnct_sort=lambda file_name: file_name != 'dump.sql')
+                        fnct_sort=lambda file_name: file_name != "dump.sql",
+                    )
                     t.seek(0)
                     return t
         else:
-            cmd.insert(-1, '--format=c')
+            cmd.insert(-1, "--format=c")
             stdout = subprocess.Popen(
-                cmd, env=env, stdin=subprocess.DEVNULL,
-                stdout=subprocess.PIPE).stdout
+                cmd, env=env, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE
+            ).stdout
             if stream:
                 shutil.copyfileobj(stdout, stream)
             else:
@@ -204,13 +211,13 @@ def _dump_addons(
         if len(dir) != 0:
             bkp_file = f"{bkp_name}_{folder}.zip"
             file_path = os.path.join(bkp_dir, bkp_file)
-            with open(file_path, 'wb') as stream:
+            with open(file_path, "wb") as stream:
                 odoo.tools.osutil.zip_dir(folder, file_path, include_dir=False)
             return file_path
 
 
 def _restore_db(db, dump_file, copy=False, neutralize_database=False):
-    _logger.info('RESTORING DB: %s', db)
+    _logger.info("RESTORING DB: %s", db)
 
     exp_drop(db)
     _create_empty_database(db)
@@ -219,25 +226,23 @@ def _restore_db(db, dump_file, copy=False, neutralize_database=False):
     with tempfile.TemporaryDirectory() as dump_dir:
         if zipfile.is_zipfile(dump_file):
             # v8 format
-            with zipfile.ZipFile(dump_file, 'r') as z:
+            with zipfile.ZipFile(dump_file, "r") as z:
                 # only extract known members!
-                filestore = [
-                    m for m in z.namelist() if m.startswith('filestore/')
-                ]
-                z.extractall(dump_dir, ['dump.sql'] + filestore)
+                filestore = [m for m in z.namelist() if m.startswith("filestore/")]
+                z.extractall(dump_dir, ["dump.sql"] + filestore)
 
                 if filestore:
-                    filestore_path = os.path.join(dump_dir, 'filestore')
+                    filestore_path = os.path.join(dump_dir, "filestore")
 
-            pg_cmd = 'psql'
-            pg_args = ['-q', '-f', os.path.join(dump_dir, 'dump.sql')]
+            pg_cmd = "psql"
+            pg_args = ["-q", "-f", os.path.join(dump_dir, "dump.sql")]
         else:
             # <= 7.0 format (raw pg_dump output)
-            pg_cmd = 'pg_restore'
-            pg_args = ['--no-owner', dump_file]
+            pg_cmd = "pg_restore"
+            pg_args = ["--no-owner", dump_file]
 
         r = subprocess.run(
-            [find_pg_tool(pg_cmd), '--dbname=' + db, *pg_args],
+            [find_pg_tool(pg_cmd), "--dbname=" + db, *pg_args],
             env=exec_pg_environ(),
             stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
@@ -250,61 +255,60 @@ def _restore_db(db, dump_file, copy=False, neutralize_database=False):
             env = odoo.api.Environment(cr, SUPERUSER_ID, {})
             if copy:
                 # if it's a copy of a database, force generation of a new dbuuid
-                env['ir.config_parameter'].init(force=True)
+                env["ir.config_parameter"].init(force=True)
             if neutralize_database:
                 odoo.modules.neutralize.neutralize_database(cr)
 
             if filestore_path:
-                filestore_dest = env['ir.attachment']._filestore()
+                filestore_dest = env["ir.attachment"]._filestore()
                 shutil.move(filestore_path, filestore_dest)
 
-    _logger.info('RESTORE DB: %s', db)
+    _logger.info("RESTORE DB: %s", db)
 
 
 def _restore_addons(dump_file, addons=""):
     cwd = os.getcwd()
-    dest = addons if addons != "" else dump_file.split('_')[-1:][0].split(
-        '.')[0]
+    dest = addons if addons != "" else dump_file.split("_")[-1:][0].split(".")[0]
     if os.path.isdir(dest):
         shutil.rmtree(dest)
     if not os.path.exists(dest):
         os.makedirs(dest)
-    with zipfile.ZipFile(dump_file, 'r') as z:
+    with zipfile.ZipFile(dump_file, "r") as z:
         z.extractall(dest)
 
 
 def list_dbs(force=False):
-    if not odoo.tools.config['list_db'] and not force:
+    if not odoo.tools.config["list_db"] and not force:
         raise odoo.exceptions.AccessDenied()
 
-    if not odoo.tools.config['dbfilter'] and odoo.tools.config['db_name']:
+    if not odoo.tools.config["dbfilter"] and odoo.tools.config["db_name"]:
         # In case --db-filter is not provided and --database is passed, Odoo will not
         # fetch the list of databases available on the postgres server and instead will
         # use the value of --database as comma seperated list of exposed databases.
-        res = sorted(
-            db.strip() for db in odoo.tools.config['db_name'].split(','))
+        res = sorted(db.strip() for db in odoo.tools.config["db_name"].split(","))
         return res
 
-    chosen_template = odoo.tools.config['db_template']
-    templates_list = tuple(set(['postgres', chosen_template]))
-    db = odoo.sql_db.db_connect('postgres')
+    chosen_template = odoo.tools.config["db_template"]
+    templates_list = tuple(set(["postgres", chosen_template]))
+    db = odoo.sql_db.db_connect("postgres")
     with closing(db.cursor()) as cr:
         try:
             cr.execute(
                 "select datname from pg_database where datdba=(select usesysid from pg_user where usename=current_user) and not datistemplate and datallowconn and datname not in %s order by datname",
-                (templates_list,))
+                (templates_list,),
+            )
             res = [odoo.tools.ustr(name) for (name,) in cr.fetchall()]
         except Exception:
-            _logger.exception('Listing databases failed:')
+            _logger.exception("Listing databases failed:")
             res = []
     return res
 
 
 def change_password(new_password):
     new_password = new_password.strip()
-    if new_password == '':
+    if new_password == "":
         return
-    ctx = CryptContext(schemes=['pbkdf2_sha512'])
+    ctx = CryptContext(schemes=["pbkdf2_sha512"])
     pw_hash = ctx.hash(new_password)
     print(pw_hash)
     return
@@ -313,18 +317,36 @@ def change_password(new_password):
 def main():
     argParser = argparse.ArgumentParser()
     argParser.add_argument(
-        "-b", "--backup", action="store_true", help="backup database")
+        "-b", "--backup", action="store_true", help="backup database"
+    )
     argParser.add_argument(
-        "-r", "--restore", action="store_true", help="restore database")
+        "-f",
+        "--destfolder",
+        action="store",
+        default="./backups",
+        help="backup destination folder",
+    )
     argParser.add_argument(
-        "-d", "--dump_file", action="store", help="database dump file")
+        "-r", "--restore", action="store_true", help="restore database"
+    )
     argParser.add_argument(
-        "-p", "--password", action="store", help="generate password hash")
+        "-d", "--dump_file", action="store", help="database dump file"
+    )
+    argParser.add_argument(
+        "-p", "--password", action="store", help="generate password hash"
+    )
+    argParser.add_argument(
+        "-c",
+        "--config",
+        action="store",
+        default="./conf/odoo.conf",
+        help="odoo.conf file location",
+    )
 
     args = argParser.parse_args()
-    odoo.tools.config._parse_config(["-c", "./conf/odoo.conf"])
+    odoo.tools.config._parse_config(["-c", args.config])
     db_name = odoo.tools.config["db_name"]
-    addons = odoo.tools.config["addons_path"].split(',')[2:]
+    addons = odoo.tools.config["addons_path"].split(",")[2:]
 
     if not args.backup and not args.restore and not args.password:
         print(argParser.print_help())
@@ -340,8 +362,8 @@ def main():
 
     if args.backup:
         bkp_name = f"{time.strftime('%Y_%m_%d_%H_%M_%S')}_{db_name}"
-        print(_dump_db(db_name, bkp_name))
-        print(_dump_addons(addons, bkp_name))
+        print(_dump_db(db_name, bkp_name, args.destfolder))
+        print(_dump_addons(addons, bkp_name, args.destfolder))
         return
 
     if args.restore and args.dump_file is None or args.dump_file == "":
