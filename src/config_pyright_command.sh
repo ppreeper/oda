53db198d7fbd0630f3a4eb597d2ff0d215118aad
Peter Preeper <ppreeper@gmail.com>
2023-08-17 14:33:46 -0600

    reorg vscode and pyright

---
+++ src/config_pyright_command.sh
@@ -0,0 +1,12 @@
+function launch_json(){
+cat <<-_EOF_ | tee pyrightconfig.json > /dev/null
+{
+  "executionEnvironments": [
+    {
+      "root": "addons",
+      "extraPaths": ["odoo", "enterprise"]
+    }
+  ]
+}
+_EOF_
+}
