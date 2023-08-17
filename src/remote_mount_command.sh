6c5e6758106efcd128ecfae48523357338009ca6
Peter Preeper <ppreeper@gmail.com>
2023-08-17 14:24:35 -0600

    add remote mount command

--- src/remote_mount_command.sh
+++ src/remote_mount_command.sh
@@ -1,4 +1,1 @@
-echo "# this file is located in 'src/remote_mount_command.sh'"
-echo "# code for 'oda remote mount' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+ssh ${args[node]} oda node mount ${args[version]} ${args[projectname]} ${args[branch]}
