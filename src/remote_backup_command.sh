9d959ff51b9f74398bd5af95989f1aa5d3c8fe09
Peter Preeper <ppreeper@gmail.com>
2023-09-01 16:41:59 -0600

    remove debug from remote backup

--- src/remote_backup_command.sh
+++ src/remote_backup_command.sh
@@ -1,12 +1,1 @@
-echo "# this file is located in 'src/remote_backup_command.sh'"
-echo "# code for 'oda remote backup' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
-
-# what project is the node attached to
-# cmd
-# PROJECT=$(ssh ${args[node]} -- cat /etc/fstab | grep addons | awk '{print $1}' | awk -F':' '{print $2}' | sed 's,/addons$,,')
-# returns
-# /share/projects/quest15/issue_285
-
 ssh ${args[node]} oda node backup
