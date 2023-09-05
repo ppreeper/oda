96031457418cfae8cc6bc388cd5c6992475f06bf
Peter Preeper <ppreeper@gmail.com>
2023-09-05 12:08:48 -0600

    add remote to node restore

--- src/node_restore_command.sh
+++ src/node_restore_command.sh
@@ -4,5 +4,5 @@ BASE=/opt/odoo
 cd ${BASE}
 for bfile in ${args[file]}
 do
-  sudo -u odoo python3 -B /usr/local/bin/oda_db.py ${REMOTE} -r -d "${bfile}"
+  echo sudo -u odoo python3 -B /usr/local/bin/oda_db.py ${REMOTE} -r -d "${bfile}"
 done
